abc9d7773eefa1da84961645705269e76f6c20a5
Steven Qian <qianxiaodong736@gmail.com>
2018-08-03 08:52:22 -0700

    New Objective Function
    The objective function is changed to
    T_ij*ln(*)/Tt_ij-Tt_ij*ln(*)/Tt_ij. The denominator is the same

--- CDmodel_Cai_3.py
+++ CDmodel_Cai_3.py
@@ -6,116 +6,103 @@ import os
 from numpy import matrix
 from numpy import linalg as LA
 
-def FG_value(T_ij, Tt_ij, D_ij, A_ij, Station_index):
-    fg_value = np.zeros(len(Station_index)*2)
-    iter = len(Station_index)
-    # add f value
-    for origin in range(iter):
-        f_value = 0
-        for dest in range(iter):
-            if origin != dest:
-                f_value += (T_ij[origin][dest]- Tt_ij[origin][dest])*np.log10(D_ij[origin][dest])
-        fg_value[origin] = f_value
-    # add g value
-    for origin in range(iter):
-        g_value = 0
-        for dest in range(iter):
-            if origin != dest:
-                g_value += (T_ij[origin][dest]- Tt_ij[origin][dest])*np.log10(A_ij[origin][dest])
-        fg_value[origin + iter] = g_value
+def FG_value(T_ij, Tt_ij, D_ij, A_ij, origin_station, dest_station):
+    den_1 = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                den_1 += T_ij[origin][dest] * np.log10(D_ij[origin][dest])
+
+    den_2 = sum(sum(T_ij))
+
+    den_3 = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                den_3 += Tt_ij[origin][dest] * np.log10(D_ij[origin][dest])
+
+    den_4 = sum(sum(Tt_ij))
+
+    f_value = den_1/den_4-den_3/den_4
+
+    den_5 = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                den_5 += T_ij[origin][dest] * np.log10(A_ij[origin][dest])
+
+    den_6 = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                den_6 += Tt_ij[origin][dest] * np.log10(A_ij[origin][dest])
+
+    g_value = den_5/den_4-den_6/den_4
+
+    return [f_value,g_value]
 
 
-    return fg_value
-
-
-def Z_i_value(Dt_j,A_ij,sigma_0,D_ij,beta_0,Station_index):
-    Z_i = np.zeros((len(Station_index), 1))
-    for origin in range(len(Station_index)):
+def Z_i_value(Dt_j,A_ij,sigma,D_ij,beta,origin_station,dest_station):
+    Z_i = np.zeros((len(origin_station), 1))
+    for origin in range(len(origin_station)):
         z_i = 0
-        for dest in range(len(Station_index)):
-            if dest != origin:
-                #z_i += Dt_j[dest] * math.pow(A_ij[origin][dest], sigma_0[origin]) * math.pow(D_ij[origin][dest],beta_0[origin])
-                try:
-                    z_i += Dt_j[dest] * math.pow(A_ij[origin][dest], sigma_0[origin]) * math.pow(D_ij[origin][dest], beta_0[origin])
-                except OverflowError:
-                    z_i = np.inf
-                    break
-
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                z_i += Dt_j[dest] * math.pow(A_ij[origin][dest], sigma) * math.pow(D_ij[origin][dest], beta)
         Z_i[origin] = math.pow(z_i, -1)
     return Z_i
 
 
-def T_ij_value(Z_i,Ot_i,Dt_j,A_ij,sigma_0,D_ij,beta_0,Station_index):
-    T_ij = np.zeros((len(Station_index), len(Station_index)))
-    for origin in range(len(Station_index)):
-        for dest in range(len(Station_index)):
-            if origin != dest:
-                T_ij[origin][dest] = (Z_i[origin][0] * Ot_i[origin] * Dt_j[dest] * math.pow(A_ij[origin][dest], sigma_0[origin]) * math.pow(D_ij[origin][dest], beta_0[origin]))
-                '''
-                if Z_i[origin][0] == 0:
-                    T_ij[origin][dest] = 0
-                else:
-                    print "math.pow(A_ij[origin][dest],sigma_0[origin])"
-                    print origin, dest
-                    print math.pow(A_ij[origin][dest],sigma_0[origin])
-                    print 'math.pow(D_ij[origin][dest], beta_0[origin])'
-                    print math.pow(D_ij[origin][dest], beta_0[origin])
-                    T_ij[origin][dest] = (Z_i[origin][0] * Ot_i[origin] * Dt_j[dest] * math.pow(A_ij[origin][dest],sigma_0[origin]) * math.pow(D_ij[origin][dest], beta_0[origin]))
-                '''
+def T_ij_value(Z_i,Ot_i,Dt_j,A_ij,sigma,D_ij,beta,origin_station, dest_station):
+    T_ij = np.zeros((len(origin_station), len(dest_station)))
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                T_ij[origin][dest] = (Z_i[origin] * Ot_i[origin] * Dt_j[dest] * math.pow(A_ij[origin][dest], sigma) * math.pow(D_ij[origin][dest], beta))
     return T_ij
 
 
-def Jacobi_Matrix(T_ij,D_ij,A_ij,Station_index):
-    iter = len(Station_index)
-    jacobi_matrix = np.zeros((iter*2,iter*2), dtype = np.float32)
+def Jacobi_Matrix(T_ij,Tt_ij,D_ij,A_ij,origin_station, dest_station):
+    jacobi_matrix = np.zeros((2,2), dtype = np.float32)
     # the partial deviation
-    for origin in range(iter):
-        jacobi_matrix[origin][origin] = F_beta(T_ij,D_ij,origin,Station_index)
-        jacobi_matrix[origin][origin+iter] = F_sigma(T_ij, D_ij,A_ij, origin, Station_index)
-        jacobi_matrix[iter+origin][origin] = G_beta(T_ij, D_ij, A_ij, origin, Station_index)
-        jacobi_matrix[iter + origin][iter+ origin] = G_sigma(T_ij, A_ij, origin, Station_index)
+    den_1 = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                den_1 += T_ij[origin][dest] * (np.log10(D_ij[origin][dest]))**2
+    '''
+    den_2 = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                den_2 += T_ij[origin][dest] * np.log10(D_ij[origin][dest])
+    '''
+    den_3 = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                den_3 += T_ij[origin][dest] * np.log10(D_ij[origin][dest]) * np.log10(A_ij[origin][dest])
+    '''
+    den_4 = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                den_4 += T_ij[origin][dest] * np.log10(A_ij[origin][dest])
+    '''
+    den_5 = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                den_5 += T_ij[origin][dest] * (np.log10(A_ij[origin][dest])) ** 2
+
+    den_6 = sum(sum(Tt_ij))
+
+    jacobi_matrix[0][0] = den_1/den_6
+    jacobi_matrix[0][1] = den_3/den_6
+    jacobi_matrix[1][0] = jacobi_matrix[0][1]
+    jacobi_matrix[1][1] = den_5/den_6
 
     return jacobi_matrix
-
-
-def F_beta(T_ij,D_ij,origin,Station_index):
-    iter = len(Station_index)
-    f_beta = 0
-    for dest in range(iter):
-        if dest != origin:
-            f_beta += T_ij[origin][dest] * (np.log10(D_ij[origin][dest]))**2
-
-    return f_beta
-
-
-def F_sigma(T_ij,D_ij,A_ij,origin,Station_index):
-    iter = len(Station_index)
-    f_sigma = 0
-    for dest in range(iter):
-        if dest != origin:
-            f_sigma += T_ij[origin][dest] * np.log10(D_ij[origin][dest]) * np.log10(A_ij[origin][dest])
-
-    return f_sigma
-
-
-def G_beta(T_ij,D_ij,A_ij,origin,Station_index):
-    iter = len(Station_index)
-    g_beta = 0
-    for dest in range(iter):
-        if dest != origin:
-            g_beta += T_ij[origin][dest] * np.log10(D_ij[origin][dest]) * np.log10(A_ij[origin][dest])
-
-    return g_beta
-
-
-def G_sigma(T_ij,A_ij,origin,Station_index):
-    iter = len(Station_index)
-    g_sigma = 0
-    for dest in range(iter):
-        if dest != origin:
-            g_sigma += T_ij[origin][dest] * (np.log10(A_ij[origin][dest]))**2
-
-    return g_sigma
 
 
 def A_ij_matrix(Dt_j,D_ij,Station_index,distance,rho):
@@ -141,12 +128,36 @@ def A_ij_matrix(Dt_j,D_ij,Station_index,distance,rho):
                 A_ij[origin][dest] = a_ij
     return A_ij
 
-# read the station data
-path = '/Volumes/GoogleDrive/My Drive/Dissertation/Data/Chicago/OD'
+
+def OD_Diff(T_ij,Tt_ij,origin_station,dest_station):
+    Diff = np.zeros((len(origin_station), len(dest_station)))
+    diff = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if origin_station[origin] != dest_station[dest]:
+                if Tt_ij[origin][dest] != 0:
+                    diff = float(int(T_ij[origin][dest]) - int(Tt_ij[origin][dest])) / Tt_ij[origin][dest]
+                else:
+                    if T_ij[origin][dest] != 0:
+                        diff = np.inf
+            Diff[origin][dest] = diff
+    i = 0
+    diff_value = 0
+    for origin in range(len(origin_station)):
+        for dest in range(len(dest_station)):
+            if Diff[origin][dest] != np.inf:
+                diff_value += Diff[origin][dest]
+                i += 1
+    return diff_value / i
+
+
+#### Part 1 Data compile
+## read the station data
+path = '/Volumes/GoogleDrive/My Drive/Dissertation/Data/Chicago'
 #path = 'G:\My Drive\Dissertation\Data\Chicago\OD'
 ## build a staiton index dictionary
 Station_index = {}
-with open(os.path.join(path, 'Chi_BS_Station_2015.csv'), 'r') as f:
+with open(os.path.join(path, 'OD', 'Chi_BS_Station_2015.csv'), 'r') as f:
     reader = csv.reader(f)
     reader.next()
     dim = 0
@@ -159,151 +170,108 @@ with open(os.path.join(path, 'Chi_BS_Station_2015.csv'), 'r') as f:
 del f
 del reader
 
-# read OD matrix
-## calculate the sum(OD_ij)
-with codecs.open(os.path.join(path, 'Chi_Bikeshare_OD_2016.csv'),'r',encoding="utf-8-sig") as f:
+## read the stations in disadvantaged communities and other areas
+dis_station = []
+other_station = []
+with open(os.path.join(path, 'Station', 'Chi_Bikeshare_Ridership_Regression.csv'), 'r') as f:
+    reader = csv.reader(f)
+    reader.next()
+    for row in reader:
+        type = row[58]
+        station_id = int(row[2])
+        if type == 'Disadvantaged areas':
+            dis_station.append(station_id)
+        else:
+            other_station.append(station_id)
+del f
+del reader
+
+dis_station.sort()
+other_station.sort()
+
+origin_station = other_station
+dest_station = other_station
+
+## read OD matrix
+with codecs.open(os.path.join(path, 'OD', 'Chi_Bikeshare_OD_2016.csv'),'r',encoding="utf-8-sig") as f:
     reader = csv.reader(f)
     x = list(reader)
     Tt_ij = np.array(x).astype("float")
 for origin in range(len(Station_index)):
     Tt_ij[origin][origin] = 0
-## calculate the Ot_i and Dt_j
+
 Ot_i = np.sum(Tt_ij, axis=1)
 Dt_j = np.sum(Tt_ij, axis=0)
+## create a new matrix based on categories
+Tt_ij_origin_dest = np.zeros((len(origin_station),len(dest_station)))
+for origin in origin_station:
+    for dest in dest_station:
+        Tt_ij_origin_dest[origin_station.index(origin)][dest_station.index(dest)] = Tt_ij[Station_index[origin]][Station_index[dest]]
+Ot_i_origin = np.sum(Tt_ij_origin_dest, axis=1)
+Dt_j_dest = np.sum(Tt_ij_origin_dest, axis=0)
 
-# read distance matrix
-with codecs.open(os.path.join(path,'Chi_BS_Dist_Matrix_0_calibration_simple.csv'),'r',encoding="utf-8-sig") as f:
+## read distance matrix
+with codecs.open(os.path.join(path, 'OD', 'Chi_BS_Dist_Matrix_0_calibration_simple.csv'),'r',encoding="utf-8-sig") as f:
     reader = csv.reader(f)
     x = list(reader)
     D_ij = np.array(x).astype("float")
+## create a new distance based on categories
+D_ij_origin_dest = np.zeros((len(origin_station),len(dest_station)))
+for origin in origin_station:
+    for dest in dest_station:
+        D_ij_origin_dest[origin_station.index(origin)][dest_station.index(dest)] = D_ij[Station_index[origin]][Station_index[dest]]
 # read price matrix and we compare the parameter
 
-# Calculate the A_ij Matrix
+## Calculate the A_ij Matrix
 rho = -1
 distance = 3000
 A_ij = A_ij_matrix(Dt_j,D_ij,Station_index,distance,rho)
+## create a new A_ij based on categories
+A_ij_origin_dest = np.zeros((len(origin_station),len(dest_station)))
+for origin in origin_station:
+    for dest in dest_station:
+        A_ij_origin_dest[origin_station.index(origin)][dest_station.index(dest)] = A_ij[Station_index[origin]][Station_index[dest]]
+
 
 ##############
 #  calibration process
 ## initial value
-#beta_0 = np.asarray(np.random.normal(-1.5, 0.5, len(Station_index)))
-#sigma_0 = np.asarray(np.random.normal(2.5, 1, len(Station_index)))
-beta_0 = np.asarray([-1.5] *len(Station_index), dtype=np.float64)
-sigma_0 = np.asarray([2.5] *len(Station_index),dtype=np.float64)
-stepsize_1 = 2 # (0,1)
-stepsize_2 = 0.25 # rho and (0, 0.5)
+beta_0 = -4
+sigma_0 = 1
+stepsize = 0.1 # (0,1)
 iteration = 1
-threshold = 100
-Z_i = np.zeros((len(Station_index),1))
-T_ij = np.zeros((len(Station_index),len(Station_index)))
+threshold = 0.00000001
 
 ## keep the value change for every iteration
 fg_value_store = []
-beta_value_store = beta_0
-sigma_value_store = sigma_0
-while iteration < 1000:
+beta_value_store = [beta_0]
+sigma_value_store = [sigma_0]
+while iteration < 10000:
     print '############'
     print 'This is the ' + str(iteration) + 'th iteration'
-    Z_i = Z_i_value(Dt_j, A_ij, sigma_0, D_ij, beta_0, Station_index)
-    T_ij = T_ij_value(Z_i, Ot_i, Dt_j, A_ij, sigma_0, D_ij, beta_0, Station_index)
-    jacobi_matrix = Jacobi_Matrix(T_ij,D_ij,A_ij,Station_index)
+    Z_i = Z_i_value(Dt_j_dest, A_ij_origin_dest, sigma_0, D_ij_origin_dest, beta_0, origin_station, dest_station)
+    T_ij = T_ij_value(Z_i, Ot_i_origin, Dt_j_dest, A_ij_origin_dest, sigma_0, D_ij_origin_dest, beta_0, origin_station, dest_station)
+    jacobi_matrix = Jacobi_Matrix(T_ij, D_ij_origin_dest, A_ij_origin_dest, origin_station, dest_station)
     jacobi_matrix = matrix(jacobi_matrix)
-    var_0 = np.concatenate((beta_0, sigma_0), axis=0)
-    var_0 = matrix(var_0)
-    fg_value = FG_value(T_ij, Tt_ij, D_ij, A_ij, Station_index)
-    LA_diff = LA.norm(fg_value)
-    fg_value_store.append(LA_diff)
-    print LA_diff
-    if LA_diff > threshold:
-        try:
-            direction = - 0.01 * jacobi_matrix.I * matrix(fg_value).T
-        except:
-            [m, n] = jacobi_matrix.shape;
-            jacobi_matrix_new = jacobi_matrix
-            for index in range(m):
-                jacobi_matrix_new[index,index] += 0.1
-            direction = - 0.01 * jacobi_matrix_new.I * matrix(fg_value).T
-
-        # decide the stepsize
-        m = 0
-        fg_value_mid = LA_diff
-        while m < 8:
-            var_mid = var_0.T + (stepsize_1 ** m) * direction
-            beta_mid = np.asarray(var_mid.T)[0][0:len(beta_0)]
-            sigma_mid = np.asarray(var_mid.T)[0][len(beta_0):len(var_mid)]
-            try:
-                Z_i_mid = Z_i_value(Dt_j, A_ij, sigma_mid, D_ij, beta_mid, Station_index)
-                T_ij_mid = T_ij_value(Z_i_mid, Ot_i, Dt_j, A_ij, sigma_mid, D_ij, beta_mid, Station_index)
-                fg_value_left = LA.norm(FG_value(T_ij_mid, Tt_ij, D_ij, A_ij, Station_index))
-                print fg_value_left
-                if fg_value_left < fg_value_mid:
-                    m += 1
-                    fg_value_mid = fg_value_left
-                    print m
-                else:
-                    m -= 1
-                    break
-            except:
-                m -= 1
-                stepsize = stepsize_1 ** m
-                var_1 = var_0.T + stepsize * direction
-                beta_0 = np.asarray(var_1.T)[0][0:len(beta_0)]
-                sigma_0 = np.asarray(var_1.T)[0][len(beta_0):len(var_1)]
-                try:
-                    Z_i_value(Dt_j, A_ij, sigma_0, D_ij, beta_0, Station_index)
-                    T_ij_value(Z_i, Ot_i, Dt_j, A_ij, sigma_0, D_ij, beta_0, Station_index)
-                    break
-                except:
-                    m = -3
-                    break
-        '''
-        while m <20:
-            var_mid = var_0.T + (stepsize_1**m)*direction
-            beta_mid = np.asarray(var_mid.T)[0][0:len(beta_0)]
-            sigma_mid = np.asarray(var_mid.T)[0][len(beta_0):len(var_mid)]
-            Z_i_mid = Z_i_value(Dt_j, A_ij, sigma_mid, D_ij, beta_mid, Station_index)
-            T_ij_mid = T_ij_value(Z_i_mid, Ot_i, Dt_j, A_ij, sigma_mid, D_ij, beta_mid, Station_index)
-            fg_value_left = FG_value(T_ij_mid, Tt_ij, D_ij, A_ij, Station_index)
-            add = LA.norm((stepsize_1**m) * stepsize_2 * matrix(fg_value) * direction)
-            fg_value_right = [i + add for i in fg_value]
-            if LA.norm(fg_value_left) <= LA.norm(fg_value_right):
-                print m
-                break
-            else:
-                m += 1
-        '''
-        stepsize = stepsize_1**m
-        print stepsize
-        var_1 = var_0.T + stepsize * direction
-        beta_0 = np.asarray(var_1.T)[0][0:len(beta_0)]
-        sigma_0 = np.asarray(var_1.T)[0][len(beta_0):len(var_1)]
-        beta_value_store = np.vstack([beta_value_store, beta_0])
-        sigma_value_store = np.vstack([sigma_value_store, sigma_0])
+    var_0 = matrix([[beta_0],[sigma_0]])
+    fg_value = FG_value(T_ij, Tt_ij_origin_dest, D_ij_origin_dest, A_ij_origin_dest, origin_station, dest_station)
+    print fg_value
+    fg_value_store.append(fg_value)
+    LA_fg = LA.norm(fg_value)
+    if LA_fg > threshold:
+        direction = - jacobi_matrix.I * matrix(fg_value).T
+        var_1 = var_0 + stepsize * direction
+        beta_0 = float(var_1[0][0])
+        sigma_0 = float(var_1[1][0])
+        beta_value_store.append(beta_0)
+        sigma_value_store.append(sigma_0)
         iteration += 1
     else:
         print 'done'
         break
 
-
 # calculate the overall goodness of fit
-## calculate the O_i and D_j
+Ave_diff = OD_Diff(T_ij,Tt_ij_origin_dest,origin_station,dest_station)
 
-Diff = np.zeros((len(Station_index),len(Station_index)))
-diff = 0
-for origin in range(len(Station_index)):
-    for dest in range(len(Station_index)):
-        if origin != dest:
-            if Tt_ij[origin][dest] != 0:
-                diff = (T_ij[origin][dest] - Tt_ij[origin][dest])/Tt_ij[origin][dest]
-            else:
-                if T_ij[origin][dest] !=0:
-                    diff = np.inf
-        Diff[origin][dest] = diff
-i = 0
-diff_value = 0
-for origin in range(len(Station_index)):
-    for dest in range(len(Station_index)):
-        if Diff[origin][dest] != np.inf:
-            diff_value += Diff[origin][dest]
-            i += 1
-Ave_diff = diff_value/i
+
+        
